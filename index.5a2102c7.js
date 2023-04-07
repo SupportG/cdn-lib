e756d1cebb60008e197fb7f24005f669ea11fc4c
SupportG <ttsergey@bk.ru>
2023-04-07 15:33:53 +0600

    Update index.5a2102c7.js

--- index.5a2102c7.js
+++ index.5a2102c7.js
@@ -534,7 +534,7 @@ const Y = new d(".swiper", {
     speed: 1e3,
     effect: "gl",
     loop: !0,
-    gl: { shader: "ripple" },
+    gl: { shader: "stretch" },
     navigation: { prevEl: ".swiper-button-prev", nextEl: ".swiper-button-next" },
   }),
   I = document.querySelector(".demo-shader-picker"),
